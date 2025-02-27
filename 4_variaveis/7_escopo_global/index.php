bbb3e68aa5c21020546abb3f99eb884707037ce5
BrunoMeccaA <brumec2013@hotmail.com>
2025-02-27 20:48:12 -0300

    aula noite 27/02/2025

---
+++ 4_variaveis/7_escopo_global/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$teste = "asd";
+
+echo "$teste global 1 <br>";
+
+if (5>2){
+    $teste = "dsa";
+
+    echo "$teste if <br>";
+}
+
+echo "$teste global 2 <br>";
+
+function testandoGlobal() {
+    global $teste;
+    echo "$teste global funcao <br>";
+}
+
+testandoGlobal();
+?>
---
+++ Exercicios/ex10.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 5;
+$b = 2;
+$c = 7;
+
+$calculo = ($c - $a) / $b * $c;
+
+echo $calculo;
+
+?>
