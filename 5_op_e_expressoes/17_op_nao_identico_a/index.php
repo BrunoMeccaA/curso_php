d02e583e028f49a97f69d8d69d8ee00e7a581514
BrunoMeccaA <brumec2013@hotmail.com>
2025-03-04 20:51:29 -0300

    aula noite 04/03/2025

---
+++ 5_op_e_expressoes/17_op_nao_identico_a/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 1;
+$b = "1";
+
+if($a != $b) {
+    echo "A é diferente de B 1 <br>";
+}
+
+if($a !== $b) {
+    echo "A é diferente de B 2 <br>";
+}
+
+if($a !== 2) {
+    echo "Não é identico 1 <br>";
+}
+
+if($a !== "1") {
+    echo "Não é identico 2 <br>";
+}
+
+?>
