a9206d3e60ed6f5ef52739f1330ba53c036bb7da
BrunoMeccaA <brumec2013@hotmail.com>
2025-03-08 13:19:13 -0300

    aula manha 08/03/2025

---
+++ Exercicios/ex18.php
@@ -0,0 +1,15 @@
+<?php
+
+if(12 < 5 or "João" === "João"){
+    echo "Teste 01 <br>";
+}
+
+if(1 > 5 or 1){
+    echo "Teste 02 <br>";
+}
+
+if(20 === "20" and 51 >= 31){
+    echo "Teste 03 <br>";
+}
+
+?>
