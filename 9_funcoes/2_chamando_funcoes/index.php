55152b3a76a20e828f1345e1da510b004521a12b
BrunoMeccaA <brumec2013@hotmail.com>
2025-03-18 10:11:22 -0300

    aula manha 18/03/2025

---
+++ 9_funcoes/2_chamando_funcoes/index.php
@@ -0,0 +1,14 @@
+<?php
+// definindo funcao
+function soma(){
+    $a = 5;
+    $b = 4;
+    echo $a + $b . "<br>";
+}
+// chamando / invocando funcao
+soma();
+
+//funcao do php
+echo strtoupper("Testando Função");
+
+?>
