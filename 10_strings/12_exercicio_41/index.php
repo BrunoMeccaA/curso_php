ef330c07725d3eaadfaedc1dc38500aba8291c26
BrunoMeccaA <brumec2013@hotmail.com>
2025-03-29 20:11:36 -0300

    aula noite 29/03/2025

---
+++ 10_strings/12_exercicio_41/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$frase1 = "este intem está em ";
+$frase2 = "promoção";
+
+echo ucfirst($frase1) . strtoupper($frase2);
+
+?>
---
+++ 10_strings/14_substr/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$str = "Esta é minha string";
+$minha = substr($str, 10, 5);
+
+echo $str . "<br>";
+echo $minha . "<br>";
+
+?>
