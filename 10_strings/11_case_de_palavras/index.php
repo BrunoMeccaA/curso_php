85dda62b77ff2cd037993bda07c124d23f487ff2
BrunoMeccaA <brumec2013@hotmail.com>
2025-03-29 11:12:28 -0300

    aula manha 29/03/2025

---
+++ 10_strings/11_case_de_palavras/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$frase = "testando o case de uma palavra <br>";
+$frase2 = "Testando o case de uma palavra <br>";
+$frase3 = "testando o case de uma palavra <br>";
+
+//Primeira letra em maiusculo
+echo ucfirst($frase);
+echo ucfirst($frase);
+
+//Todas as palavras com iniciais maiusculas
+echo ucwords($frase3);
+echo ucfirst($frase2);
+
+?>
