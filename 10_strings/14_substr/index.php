ed3b4548826fc8376989f9387db08c67f08819e2
BrunoMeccaA <brumec2013@hotmail.com>
2025-04-01 10:42:46 -0300

    aula manha 01/04/2025

--- 10_strings/14_substr/index.php
+++ 10_strings/14_substr/index.php
@@ -1,9 +1,17 @@
 <?php
 
 $str = "Esta é minha string";
-$minha = substr($str, 10, 5);
+$minha = substr($str, 10, 5); // STRING PAI, INDICE INICIAL, COMPRIMENTO DA PALAVRA
 
 echo $str . "<br>";
 echo $minha . "<br>";
+
+$str2 = "Testando esta string abc";
+
+$novaString = substr($str2, 8); // OMITIR COMPRIMENTO = PEGAR ATÉ O FIM
+echo $novaString . "<br>";
+
+$novaString2 = substr($str2, 8, -3); // COMPRIMENTO NEGATIVO = REMOVER DO ULTIMO INDICE
+echo $novaString2 . "<br>";
 
 ?>
---
+++ 10_strings/15_exercicio_42/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$frase = "Cade o meu queijo? Ele estava aqui em cima";
+
+$str1 = substr($frase, 11, 6);
+echo $str1 . "<br>";
+
+$str2 = substr($frase, 23, 6);
+echo $str2 . "<br>";
+
+?>
