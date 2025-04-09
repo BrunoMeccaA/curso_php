caddd02928891e0454d550fec26b6f8e7b3d3977
BrunoMeccaA <brumec2013@hotmail.com>
2025-04-08 21:00:53 -0300

    aula noite 08/04/2025

---
+++ 11_arrays/9_exercicio_47/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$carro = ["jaguar", 3.0, "azul", 18, "Teto Solar", "automático"];
+
+list ($marca, $motor, $cor, $roda, $teto, $cambio) = $carro;
+
+echo "$marca <br>";
+echo "$motor <br>";
+echo "$cor <br>";
+echo "$roda <br>";
+echo "$teto <br>";
+echo "$cambio <br>";
+?>
